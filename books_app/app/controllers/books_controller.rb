25c9d196ec929ce92db2a32f8902b30f7f0020ee
girik <girikaustuv@gmail.com>
2021-11-29 18:47:56 +0545

    Author Added

--- books_app/app/controllers/books_controller.rb
+++ books_app/app/controllers/books_controller.rb
@@ -65,5 +65,7 @@ class BooksController < ApplicationController
     # Only allow a list of trusted parameters through.
     def book_params
       params.require(:book).permit(:title, :memo)
-    end
+      params.require(:book).permit(:title, :memo, :author)
+      end
+      
 end
